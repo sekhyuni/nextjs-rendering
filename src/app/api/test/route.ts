f0ceb10f6538aca48d0f40f20474a8ac642a443f
sekhyuni <sekhyuni@gmail.com>
2024-01-10 22:04:52 +0900

    feat: Route Handlers에 force-dynamic 추가

--- src/app/api/test/route.ts
+++ src/app/api/test/route.ts
@@ -1,6 +1,8 @@
 import { promises as fs } from 'fs';
 import { NextResponse } from 'next/server';
 
+export const dynamic = 'force-dynamic';
+
 export async function GET() {
   const response = await fs.readFile(`${process.cwd()}/something.txt`, 'utf-8');
 
